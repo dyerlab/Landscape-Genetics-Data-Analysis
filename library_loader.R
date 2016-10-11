40648371c3e146ed9781c980a2fddda3de55c104
dyerlab <rjdyer@vcu.edu>
2016-10-11 15:17:36 -0400

    library loader

--- library_loader.R
+++ library_loader.R
@@ -17,26 +17,48 @@
 #   source this file and the libraries will be downloaded and installed.
 
 # To load this file, execute the following code in an R session.
+# install.packages("devtools")
 # url <- "https://raw.githubusercontent.com/dyerlab/Landscape-Genetics-Data-Analysis/master/library_loader.R"
 # library(devtools)
 # source_url( url )
+
+
 
 files_from_cran <- c("cowplot",
                      "DT",
                      "devtools",
                      "GGally",
+                     "gdistance",
                      "ggmap",
                      "ggplot2",
+                     "ggrepel",
                      "igraph",
+                     "kinship2",
+                     "knitr",
                      "maps",
                      "networkD3",
                      "raster",
                      "rasterVis",
+                     "RColorBrewer",
+                     "RCurl",
                      "rgeos",
                      "rgdal",
+                     "rlidar",
                      "rmarkdown",
-                     "SDMTools")
+                     "SDMTools",
+                     "sp",
+                     "vegan")
 
 need_inst <- setdiff( files_from_cran, rownames(installed.packages()) )
 if( length( need_inst ) ) 
   install.packages( need_inst, dependencies = TRUE)
+
+library(devtools)
+install_github("dyerlab/gstudio")
+install_github("dyerlab/popgraph")
+
+print("Download STRUCTURE from the Pritchard Laboratory webpage")
+system("open http://pritchardlab.stanford.edu/structure.html")
+
+print("Download Circuitscape from Brad McRae's webpage")
+system("open http://www.circuitscape.org")
